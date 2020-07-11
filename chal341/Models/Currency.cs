5839e99d44958a1d9687f9021e81873e54a741fe
vieira-andre <andre.vieira@outlook.com>
2020-07-11 17:22:30 -0300

    Create Currency class

---
+++ chal341/Models/Currency.cs
@@ -0,0 +1,12 @@
+﻿namespace chal341.Models
+{
+    public class Currency
+    {
+        public Currency(string code)
+        {
+            Code = code;
+        }
+
+        public string Code { get; private set; }
+    }
+}
