35f7184f9d2642f7281d019e9609deb44d91e14c
vieira-andre <andre.vieira@outlook.com>
2020-07-11 17:22:44 -0300

    Create ExchangeRate class

---
+++ chal341/Models/ExchangeRate.cs
@@ -0,0 +1,17 @@
+﻿namespace chal341.Models
+{
+    public class ExchangeRate
+    {
+        private (Currency baseCurrency, Currency quoteCurrency) currencyPair;
+
+        public ExchangeRate(string foreignCurrency, string homeCurrency, decimal rate)
+        {
+            currencyPair.baseCurrency = new Currency(foreignCurrency);
+            currencyPair.quoteCurrency = new Currency(homeCurrency);
+            Rate = rate;
+        }
+
+        public (Currency baseCurrency, Currency quoteCurrency) CurrencyPair { get => currencyPair; private set => currencyPair = value; }
+        public decimal Rate { get; private set; }
+    }
+}
