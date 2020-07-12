99b3a47d34ace8d15d8dda96c4c6956cab592572
vieira-andre <andre.vieira@outlook.com>
2020-07-12 20:05:26 -0300

    Create repo class for exchange fee

---
+++ chal341/Repositories/ExchangeFeeRepository.cs
@@ -0,0 +1,28 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using chal341.Models;
+using chal341.Models.Data;
+using System.Threading.Tasks;
+
+namespace chal341.Repositories
+{
+    public class ExchangeFeeRepository : IExchangeFeeRepository
+    {
+        private readonly DynamoDBContext _context;
+
+        public ExchangeFeeRepository(IAmazonDynamoDB dymanoDbClient)
+        {
+            _context = new DynamoDBContext(dymanoDbClient);
+        }
+
+        public async Task AddExchangeFeeAsync(ExchangeFeeDb exchangeFeeDb)
+        {
+            await _context.SaveAsync(exchangeFeeDb);
+        }
+
+        public Task<ExchangeFeeDb> GetExchangeFeeAsync(ClientSegment segment)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
