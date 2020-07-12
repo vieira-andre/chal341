bb841d3f9dd639bcce547d72e2d631aa19c58d04
vieira-andre <andre.vieira@outlook.com>
2020-07-12 20:14:14 -0300

    Create extension method to parse string to decimal following desired criteria

---
+++ chal341/Extensions/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace chal341.Extensions
+{
+    public static class Extensions
+    {
+        public static decimal ToDecimal(this string decimalAsString)
+        {
+            if (decimal.TryParse(decimalAsString, NumberStyles.AllowDecimalPoint, new CultureInfo("pt-BR"), out decimal result))
+                return result;
+            else
+                throw new FormatException("The string cannot be parsed to decimal. " +
+                    "If a decimal point was provided, it must be in accordance with the current culture info. " +
+                    "Likewise, no thousands separator is allowed.");
+        }
+    }
+}
