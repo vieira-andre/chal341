318d52d2c0dab3b9662ccab9f92871221d752482
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:48:13 -0300

    Implement ctor and validation for segment param

--- chal341/Models/ExchangeFee.cs
+++ chal341/Models/ExchangeFee.cs
@@ -1,9 +1,25 @@
-﻿namespace chal341.Models
+﻿using System;
+
+namespace chal341.Models
 {
     public class ExchangeFee : IExchangeFee
     {
+        public ExchangeFee(string segment, decimal feeCharged)
+        {
+            ValidateSegment(segment);
+            FeeCharged = feeCharged;
+        }
+
         public ClientSegment Segment { get; private set; }
 
         public decimal FeeCharged { get; private set; }
+
+        private void ValidateSegment(string segment)
+        {
+            if (Enum.TryParse(segment, out ClientSegment result))
+                Segment = result;
+            else
+                throw new ArgumentException("The provided value is not a valid client segment.");
+        }
     }
 }
