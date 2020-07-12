4e00ee2a6783248cb5e2132f4a2ad5e4d1ab2f1c
vieira-andre <andre.vieira@outlook.com>
2020-07-12 08:13:41 -0300

    Create validator for SetExchangeFeeRequest

---
+++ chal341/Validators/SetExchangeFeeRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using chal341.Contracts;
+using FluentValidation;
+
+namespace chal341.Validators
+{
+    public class SetExchangeFeeRequestValidator : AbstractValidator<SetExchangeFeeRequest>
+    {
+        public SetExchangeFeeRequestValidator()
+        {
+            RuleFor(x => x.Segment)
+                .NotEmpty();
+
+            RuleFor(x => x.Fee)
+                .NotNull();
+        }
+    }
+}
