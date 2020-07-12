9fb29b850e6b6af772080fda6ab894f0988abbd8
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:51:13 -0300

    Create SetExchangeFeeRequest contract

---
+++ chal341/Contracts/Requests/SetExchangeFeeRequest.cs
@@ -0,0 +1,11 @@
+﻿using chal341.Models;
+
+namespace chal341.Contracts
+{
+    public class SetExchangeFeeRequest
+    {
+        public ClientSegment Segment { get; set; }
+
+        public decimal Fee { get; set; }
+    }
+}
