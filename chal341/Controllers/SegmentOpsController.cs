c3c0988871c2687282bfe54ac2056c7a1ce3e009
vieira-andre <andre.vieira@outlook.com>
2020-07-12 20:08:36 -0300

    Inject segment ops service into controller

--- chal341/Controllers/SegmentOpsController.cs
+++ chal341/Controllers/SegmentOpsController.cs
@@ -1,6 +1,7 @@
 ﻿using chal341.Contracts;
-using chal341.Models;
+using chal341.Services;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -11,6 +12,13 @@ namespace chal341.Controllers
     [Produces("application/json")]
     public class SegmentOpsController : ControllerBase
     {
+        private readonly ISegmentOpsService _segmentOpsService;
+
+        public SegmentOpsController(ISegmentOpsService segmentOpsService)
+        {
+            _segmentOpsService = segmentOpsService;
+        }
+
         [HttpGet("fee/{Segment}")]
         public async Task<GetExchangeFeeResponse> GetExchangeFeeAsync([FromRoute] GetExchangeFeeRequest request)
         {
