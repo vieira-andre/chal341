428a641599c59cb49e891eb58d2982483f62313b
vieira-andre <andre.vieira@outlook.com>
2020-07-12 16:58:44 -0300

    Define the data model for exchange fee

---
+++ chal341/Models/Data/ExchangeFeeDb.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace chal341.Models.Data
+{
+    [DynamoDBTable("ExchangeFee")]
+    public class ExchangeFeeDb
+    {
+        [DynamoDBHashKey]
+        public ClientSegment Segment { get; set; }
+
+        public decimal FeeCharged { get; set; }
+    }
+}
