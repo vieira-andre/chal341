3745d813336890315a0ec1012c17e0b622ad22e4
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:52:10 -0300

    Create GetExchangeFeeResponse contract

---
+++ chal341/Contracts/Responses/GetExchangeFeeResponse.cs
@@ -0,0 +1,11 @@
+﻿using chal341.Models;
+
+namespace chal341.Contracts
+{
+    public class GetExchangeFeeResponse
+    {
+        public ClientSegment Segment { get; set; }
+
+        public decimal FeeCharged { get; set; }
+    }
+}
