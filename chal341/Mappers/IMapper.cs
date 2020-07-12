ea7e4478975d0cbc22a47e19befd4fcace640ed7
vieira-andre <andre.vieira@outlook.com>
2020-07-12 16:59:14 -0300

    Create mapper between contract and dbmodel for exchange fee

---
+++ chal341/Mappers/IMapper.cs
@@ -0,0 +1,12 @@
+﻿using chal341.Contracts;
+using chal341.Models.Data;
+
+namespace chal341.Mappers
+{
+    public interface IMapper
+    {
+        GetExchangeFeeResponse ToGetExchangeFeeContract(ExchangeFeeDb exchangeFee);
+
+        ExchangeFeeDb ToExchangeFeeDbModel(SetExchangeFeeRequest setExchangeFeeRequest);
+    }
+}
---
+++ chal341/Mappers/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using chal341.Contracts;
+using chal341.Models.Data;
+
+namespace chal341.Mappers
+{
+    public class Mapper : IMapper
+    {
+        public GetExchangeFeeResponse ToGetExchangeFeeContract(ExchangeFeeDb exchangeFeeDb)
+        {
+            return new GetExchangeFeeResponse
+            {
+                Segment = exchangeFeeDb.Segment,
+                FeeCharged = exchangeFeeDb.FeeCharged
+            };
+        }
+
+        public ExchangeFeeDb ToExchangeFeeDbModel(SetExchangeFeeRequest setExchangeFeeRequest)
+        {
+            return new ExchangeFeeDb
+            {
+                Segment = setExchangeFeeRequest.Segment,
+                FeeCharged = (decimal)setExchangeFeeRequest.Fee
+            };
+        }
+    }
+}
