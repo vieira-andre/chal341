ee94b269cb5becfc7ce254ebaeff62dcf6c5538e
vieira-andre <andre.vieira@outlook.com>
2020-07-12 08:15:48 -0300

    Create model and response for errors

---
+++ chal341/Contracts/Responses/ErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿using chal341.Models;
+using System.Collections.Generic;
+
+namespace chal341.Contracts
+{
+    public class ErrorResponse
+    {
+        public IList<ErrorModel> Errors { get; set; } = new List<ErrorModel>();
+    }
+}
---
+++ chal341/Models/ErrorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace chal341.Models
+{
+    public class ErrorModel
+    {
+        public string FieldName { get; set; }
+
+        public string Message { get; set; }
+    }
+}
