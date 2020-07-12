ea9d15dcd8a0f09e5796ddbc488fadd7844ceb22
vieira-andre <andre.vieira@outlook.com>
2020-07-12 20:04:17 -0300

    Create interface for exchange fee repo

---
+++ chal341/Repositories/IExchangeFeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using chal341.Models;
+using chal341.Models.Data;
+using System.Threading.Tasks;
+
+namespace chal341.Repositories
+{
+    public interface IExchangeFeeRepository
+    {
+        Task AddExchangeFeeAsync(ExchangeFeeDb exchangeFeeDb);
+
+        Task<ExchangeFeeDb> GetExchangeFeeAsync(ClientSegment segment);
+    }
+}
