9020f59d2f8830dc054192570551bb580589318e
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:59:28 -0300

    Draft APIs for segment ops

--- chal341/Controllers/SegmentOpsController.cs
+++ chal341/Controllers/SegmentOpsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using chal341.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace chal341.Controllers
 {
@@ -6,6 +10,22 @@ namespace chal341.Controllers
     [Route("api/segment")]
     public class SegmentOpsController : ControllerBase
     {
+        [HttpGet]
+        public async Task<GetExchangeFeeResponse> GetExchangeFeeAsync([FromRoute] GetExchangeFeeRequest request)
+        {
+            throw new NotImplementedException();
+        }
 
+        [HttpGet]
+        public async Task<IList<GetExchangeFeeResponse>> GetExchangeFeesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task<GetExchangeFeeResponse> SetExchangeFeeAsync([FromBody] SetExchangeFeeRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
