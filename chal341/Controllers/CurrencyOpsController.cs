a759c255dd88f1908e2e0b91c51eb7e4ee8bf197
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:54:32 -0300

    Create CurrencyOpsController

---
+++ chal341/Controllers/CurrencyOpsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace chal341.Controllers
+{
+    [ApiController]
+    [Route("api/currency")]
+    public class CurrencyOpsController : ControllerBase
+    {
+
+    }
+}
