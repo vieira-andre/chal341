99e11c7df0fa6d8f0be6dd57c9aa6631b63b29d7
vieira-andre <andre.vieira@outlook.com>
2020-07-12 20:07:17 -0300

    Create SegmentOpsService

---
+++ chal341/Services/SegmentOpsService.cs
@@ -0,0 +1,33 @@
+﻿using chal341.Contracts;
+using chal341.Mappers;
+using chal341.Models;
+using chal341.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace chal341.Services
+{
+    public class SegmentOpsService : ISegmentOpsService
+    {
+        private readonly IExchangeFeeRepository _exchangeFeeRepository;
+        private readonly IMapper _map;
+
+        public SegmentOpsService(IExchangeFeeRepository exchangeFeeRepository, IMapper map)
+        {
+            _exchangeFeeRepository = exchangeFeeRepository;
+            _map = map;
+        }
+
+        public async Task AddExchangeFeeAsync(SetExchangeFeeRequest request)
+        {
+            var exchangeFeeDb = _map.ToExchangeFeeDbModel(request);
+
+            await _exchangeFeeRepository.AddExchangeFeeAsync(exchangeFeeDb);
+        }
+
+        public Task<GetExchangeFeeResponse> GetExchangeFeeDbAsync(ClientSegment segment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
