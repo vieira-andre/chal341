7464b07b7006eafed9bd2076c3a721e28d1ecb04
vieira-andre <andre.vieira@outlook.com>
2020-07-12 08:11:58 -0300

    Create validator class for GetExchangeFeeRequest

---
+++ chal341/Validators/GetExchangeFeeRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using chal341.Contracts;
+using FluentValidation;
+
+namespace chal341.Validators
+{
+    public class GetExchangeFeeRequestValidator : AbstractValidator<GetExchangeFeeRequest>
+    {
+        public GetExchangeFeeRequestValidator()
+        {
+            RuleFor(x => x.Segment)
+                .NotEmpty();
+        }
+    }
+}
