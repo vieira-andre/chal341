d58cde8f004ba93000c7e2156b25d43b75c1c5a3
vieira-andre <andre.vieira@outlook.com>
2020-07-12 13:39:41 -0300

    Override OnResultExecuting in validation filter

--- chal341/Filters/ValidationFilter.cs
+++ chal341/Filters/ValidationFilter.cs
@@ -1,9 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using chal341.Contracts;
+using chal341.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System.Net;
 
 namespace chal341.Filters
 {
     public class ValidationFilter : ActionFilterAttribute
     {
-        
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var errorsInModelState = context.ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Errors.Select(x => x.ErrorMessage)).ToArray();
+
+                var errorResponse = new ErrorResponse();
+
+                foreach (var error in errorsInModelState)
+                {
+                    foreach (var subError in error.Value)
+                    {
+                        var errorModel = new ErrorModel
+                        {
+                            FieldName = error.Key,
+                            Message = subError
+                        };
+
+                        errorResponse.Errors.Add(errorModel);
+                    }
+                }
+
+                context.Result = new BadRequestObjectResult(errorResponse);
+            }
+
+            if (context.HttpContext.Response.StatusCode == (int)HttpStatusCode.NoContent)
+                context.Result = new NoContentResult();
+
+            base.OnResultExecuting(context);
+        }
     }
 }
