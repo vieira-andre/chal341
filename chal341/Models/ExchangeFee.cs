d8e6fa63e0911d08d54bb64366d5bffceabd9982
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:47:33 -0300

    Create ExchangeFee class

---
+++ chal341/Models/ExchangeFee.cs
@@ -0,0 +1,9 @@
+﻿namespace chal341.Models
+{
+    public class ExchangeFee : IExchangeFee
+    {
+        public ClientSegment Segment { get; private set; }
+
+        public decimal FeeCharged { get; private set; }
+    }
+}
