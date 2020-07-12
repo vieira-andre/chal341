e693b1828f06a97516bd9b166f526ad6a807cd95
vieira-andre <andre.vieira@outlook.com>
2020-07-12 07:43:48 -0300

    Create ClientSegment enum

---
+++ chal341/Models/ClientSegment.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace chal341.Models
+{
+    public enum ClientSegment
+    {
+        Undefined,
+        [Description("Retail")]
+        RETL,
+        [Description("Personnalité")]
+        PSNL,
+        [Description("Private")]
+        PRIV
+    }
+}
