511ed2ba83963e3bbb26f1bd25ef1b445c633ccf
vieira-andre <andre.vieira@outlook.com>
2020-07-12 21:19:43 -0300

    Retrieve env variables for exchange rate API

--- chal341/Variables.cs
+++ chal341/Variables.cs
@@ -4,6 +4,10 @@ namespace chal341
 {
     public static class Variables
     {
+        public static string ExchangeApiUrl => Environment.GetEnvironmentVariable("ExchangeApiUrl") ?? ThrowExceptionForMissingVariable(nameof(ExchangeApiUrl));
+        
+        public static string ExchangeApiPath => Environment.GetEnvironmentVariable("ExchangeApiPath") ?? ThrowExceptionForMissingVariable(nameof(ExchangeApiPath));
+
         public static string HomeCurrency => Environment.GetEnvironmentVariable("HomeCurrency") ?? ThrowExceptionForMissingVariable(nameof(HomeCurrency));
 
         public static string Locale => Environment.GetEnvironmentVariable("Locale") ?? ThrowExceptionForMissingVariable(nameof(Locale));
