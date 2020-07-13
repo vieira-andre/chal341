aed742633ceffdf6f27535a00b92da5a1fc4f845
vieira-andre <andre.vieira@outlook.com>
2020-07-13 03:55:57 -0300

    Set ctor for currency ops controller

--- chal341/Controllers/CurrencyOpsController.cs
+++ chal341/Controllers/CurrencyOpsController.cs
@@ -1,4 +1,5 @@
 ﻿using chal341.Contracts;
+using chal341.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -9,10 +10,23 @@ namespace chal341.Controllers
     [Produces("application/json")]
     public class CurrencyOpsController : ControllerBase
     {
+        private readonly ICurrencyOpsService _currencyOpsService;
+
+        public CurrencyOpsController(ICurrencyOpsService currencyOpsService)
+        {
+            _currencyOpsService = currencyOpsService;
+        }
+
         [HttpGet("price")]
-        public async Task<ActionResult<GetPriceQuotationResponse>> GetPriceQuotation([FromQuery] GetPriceQuotationRequest request)
+        public async Task<ActionResult<GetPriceQuotationResponse>> GetPriceQuotationAsync([FromQuery] GetPriceQuotationRequest request)
         {
             return Ok();
+        }
+
+        [HttpGet("prices")]
+        public async Task<ActionResult<GetPriceQuotationResponse>> GetPriceQuotation([FromQuery] GetAllPriceQuotationsRequest request)
+        {
+            return Ok("All quotes");
         }
     }
 }
