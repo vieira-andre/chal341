d92e7a079b3d25c87997e7cd235d4fb22859d874
vieira-andre <andre.vieira@outlook.com>
2020-07-13 04:49:42 -0300

    Create GetExchangeRate and RetrieveExchangeRateFromApi methods

--- chal341/Services/CurrencyOpsService.cs
+++ chal341/Services/CurrencyOpsService.cs
@@ -1,7 +1,9 @@
 ﻿using chal341.Contracts;
 using chal341.Mappers;
+using chal341.Models;
 using chal341.Repositories;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace chal341.Services
@@ -19,9 +21,36 @@ namespace chal341.Services
             _map = map;
         }
 
-        public Task<GetPriceQuotationResponse> GetPriceQuotationAsync(GetPriceQuotationRequest request)
+        public async Task<GetPriceQuotationResponse> GetPriceQuotationAsync(GetPriceQuotationRequest request)
         {
-            throw new System.NotImplementedException();
+            var exchangeRate = await GetExchangeRate(request.Code);
+
+            return default;
+        }
+
+        private async Task<ExchangeRate> GetExchangeRate(string baseCurrencyCode)
+        {
+            decimal rate = await RetrieveExchangeRateFromApi(baseCurrencyCode);
+
+            return new ExchangeRate(baseCurrencyCode, Variables.HomeCurrency, rate);
+        }
+
+        private async Task<decimal> RetrieveExchangeRateFromApi(string baseCurrencyCode)
+        {
+            string path = string.Format(Variables.ExchangeApiPath, baseCurrencyCode, Variables.HomeCurrency);
+
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, string.Concat(Variables.ExchangeApiUrl, path));
+
+            var httpClient = _clientFactory.CreateClient();
+
+            var httpResponse = await httpClient.SendAsync(httpRequest);
+
+            string responseAsString = await httpResponse.Content.ReadAsStringAsync();
+
+            ExchangeApiResponse apiResponse = JsonSerializer.Deserialize<ExchangeApiResponse>(responseAsString,
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            return apiResponse.Rates.BRL;
         }
     }
 }
