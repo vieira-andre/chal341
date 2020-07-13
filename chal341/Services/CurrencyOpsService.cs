bf5c21849a3ed493e2c50df2e93f981932164a03
vieira-andre <andre.vieira@outlook.com>
2020-07-13 03:23:17 -0300

    Draft CurrencyOpsService class and set its interface

---
+++ chal341/Services/CurrencyOpsService.cs
@@ -0,0 +1,13 @@
+﻿using chal341.Contracts;
+using System.Threading.Tasks;
+
+namespace chal341.Services
+{
+    public class CurrencyOpsService : ICurrencyOpsService
+    {
+        public Task<GetPriceQuotationResponse> GetPriceQuotationAsync(GetPriceQuotationRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ chal341/Services/ICurrencyOpsService.cs
@@ -0,0 +1,10 @@
+﻿using chal341.Contracts;
+using System.Threading.Tasks;
+
+namespace chal341.Services
+{
+    public interface ICurrencyOpsService
+    {
+        Task<GetPriceQuotationResponse> GetPriceQuotationAsync(GetPriceQuotationRequest request);
+    }
+}
