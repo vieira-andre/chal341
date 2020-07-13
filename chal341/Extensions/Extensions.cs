d2ef28bdd7c15c8b155d7f84b98b0d2faed1e171
vieira-andre <andre.vieira@outlook.com>
2020-07-12 21:15:11 -0300

    Insert ref for locale env variavle in ToDecimal exception msg

--- chal341/Extensions/Extensions.cs
+++ chal341/Extensions/Extensions.cs
@@ -7,11 +7,11 @@ namespace chal341.Extensions
     {
         public static decimal ToDecimal(this string decimalAsString)
         {
-            if (decimal.TryParse(decimalAsString, NumberStyles.AllowDecimalPoint, new CultureInfo("pt-BR"), out decimal result))
+            if (decimal.TryParse(decimalAsString, NumberStyles.AllowDecimalPoint, new CultureInfo(Variables.Locale), out decimal result))
                 return result;
             else
                 throw new FormatException("The string cannot be parsed to decimal. " +
-                    "If a decimal point was provided, it must be in accordance with the current culture info. " +
+                    $"If a decimal point was provided, it must be in accordance with the current culture info ({Variables.Locale}). " +
                     "Likewise, no thousands separator is allowed.");
         }
     }
