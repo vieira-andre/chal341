e29026202acc0e915b7df642b79d8f30787c88a9
vieira-andre <andre.vieira@outlook.com>
2020-07-13 00:57:48 -0300

    Create mappings for get exchange fee

--- chal341/Mappers/IMapper.cs
+++ chal341/Mappers/IMapper.cs
@@ -1,11 +1,13 @@
 ﻿using Amazon.DynamoDBv2.DocumentModel;
 using chal341.Contracts;
-using chal341.Models.Data;
 
 namespace chal341.Mappers
 {
     public interface IMapper
     {
         Document ToDocumentModel(AddExchangeFeeRequest request);
+
+        Document ToDocumentModel(GetExchangeFeeRequest request);
+        GetExchangeFeeResponse ToContract(Document item);
     }
 }
--- chal341/Mappers/Mapper.cs
+++ chal341/Mappers/Mapper.cs
@@ -6,12 +6,29 @@ namespace chal341.Mappers
 {
     public class Mapper : IMapper
     {
+        public GetExchangeFeeResponse ToContract(Document item)
+        {
+            return new GetExchangeFeeResponse
+            {
+                Segment = item["ClientSegment"].AsString().ParseFromString(),
+                FeeCharged = item["FeeCharged"].AsString().ToDecimal()
+            };
+        }
+
         public Document ToDocumentModel(AddExchangeFeeRequest request)
         {
             return new Document
             {
                 ["ClientSegment"] = request.Segment.ToString(),
                 ["FeeCharged"] = request.Fee.ToDecimal()
+            };
+        }
+
+        public Document ToDocumentModel(GetExchangeFeeRequest request)
+        {
+            return new Document
+            {
+                ["ClientSegment"] = request.Segment.ToString()
             };
         }
     }
