8243abc176196f5e3b1f8e054ce8788942fa8678
vieira-andre <andre.vieira@outlook.com>
2020-07-12 21:13:57 -0300

    Create static class for env variables initialization

---
+++ chal341/Variables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace chal341
+{
+    public static class Variables
+    {
+        public static string HomeCurrency => Environment.GetEnvironmentVariable("HomeCurrency") ?? ThrowExceptionForMissingVariable(nameof(HomeCurrency));
+
+        public static string Locale => Environment.GetEnvironmentVariable("Locale") ?? ThrowExceptionForMissingVariable(nameof(Locale));
+
+        private static string ThrowExceptionForMissingVariable(string variableName)
+        {
+            throw new ArgumentNullException($"Missing environment variable: ${variableName}");
+        }
+    }
+}
