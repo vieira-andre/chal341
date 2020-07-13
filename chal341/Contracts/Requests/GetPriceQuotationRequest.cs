7cde70edd9f2f62eb48f3b6aeb83a84a9c5e05da
vieira-andre <andre.vieira@outlook.com>
2020-07-13 03:10:01 -0300

    Create price quotation contracts

---
+++ chal341/Contracts/Requests/GetPriceQuotationRequest.cs
@@ -0,0 +1,13 @@
+﻿using chal341.Models;
+
+namespace chal341.Contracts
+{
+    public class GetPriceQuotationRequest
+    {
+        public string Code { get; set; }
+
+        public int Units { get; set; }
+
+        public ClientSegment Segment { get; set; }
+    }
+}
---
+++ chal341/Contracts/Responses/GetPriceQuotationResponse.cs
@@ -0,0 +1,13 @@
+﻿using chal341.Models;
+
+namespace chal341.Contracts
+{
+    public class GetPriceQuotationResponse
+    {
+        public string PriceQuotation { get; set; }
+
+        public string Units { get; set; }
+
+        public ClientSegment Segment { get; set; }
+    }
+}
