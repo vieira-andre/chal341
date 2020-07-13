56188c3b4f94c272ea223365cab1b81f948d87b4
vieira-andre <andre.vieira@outlook.com>
2020-07-13 04:48:57 -0300

    Create class to map API response

---
+++ chal341/Contracts/Responses/ExchangeApiResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace chal341.Contracts
+{
+    public class ExchangeApiResponse
+    {
+        public Rates Rates { get; set; }
+        public string Base { get; set; }
+        public string Date { get; set; }
+    }
+
+    public class Rates
+    {
+        [JsonPropertyName("BRL")]
+        public decimal BRL { get; set; }
+    }
+
+}
