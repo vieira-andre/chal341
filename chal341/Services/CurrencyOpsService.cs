fe468febf6d45f76524d0c2b5f39e073a212c6bd
vieira-andre <andre.vieira@outlook.com>
2020-07-13 03:30:52 -0300

    Create currency ops service ctor

--- chal341/Services/CurrencyOpsService.cs
+++ chal341/Services/CurrencyOpsService.cs
@@ -1,10 +1,24 @@
 ﻿using chal341.Contracts;
+using chal341.Mappers;
+using chal341.Repositories;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace chal341.Services
 {
     public class CurrencyOpsService : ICurrencyOpsService
     {
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IExchangeFeeRepository _exchangeFeeRepository;
+        private readonly IMapper _map;
+
+        public CurrencyOpsService(IHttpClientFactory clientFactory, IExchangeFeeRepository exchangeFeeRepository, IMapper map)
+        {
+            _clientFactory = clientFactory;
+            _exchangeFeeRepository = exchangeFeeRepository;
+            _map = map;
+        }
+
         public Task<GetPriceQuotationResponse> GetPriceQuotationAsync(GetPriceQuotationRequest request)
         {
             throw new System.NotImplementedException();
