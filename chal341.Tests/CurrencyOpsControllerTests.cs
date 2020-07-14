e1dece7a14414d101cae8d78956fc8066d2109b8
vieira-andre <andre.vieira@outlook.com>
2020-07-14 02:27:57 -0300

    Create test project and draft first class

---
+++ chal341.Tests/CurrencyOpsControllerTests.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace chal341.Tests
+{
+    public class CurrencyOpsControllerTests
+    {
+        [Fact]
+        public async Task GetPriceQuotationAsync_ShouldReturnPriceQuotation_WhenTheSegmentFeeExists()
+        {
+
+        }
+    }
+}
